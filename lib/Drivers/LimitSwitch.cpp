bd5474db0206cb49fe98a700bf66907bd46b17af
winterbloom <23458874+winterbloom@users.noreply.github.com>
2022-08-22 11:29:32 -0700

    Adds limit switch driver

---
+++ lib/Drivers/LimitSwitch.cpp
@@ -0,0 +1,26 @@
+#include "LimitSwitch.h"
+#include <Arduino.h>
+
+using namespace lattice;
+
+LimitSwitch::LimitSwitch(const int pin) : mPin(pin), mState(false), mLast(false) {
+    pinMode(mPin, INPUT);
+	digitalWrite(mPin, HIGH);
+}
+
+void LimitSwitch::Update() {
+    mLast = mState;
+    mState = digitalRead(mPin) == LOW;
+}
+
+bool LimitSwitch::Get() {
+    return mState;
+}
+
+bool LimitSwitch::Pushed() {
+    return mState && !mLast;
+}
+
+bool LimitSwitch::Released() {
+    return !mState && mLast;
+}
---
+++ lib/Drivers/LimitSwitch.h
@@ -0,0 +1,41 @@
+namespace lattice {
+class LimitSwitch {
+   public:
+    /**
+     * Sets up to read a limit switch input from the given pin
+     * (pulled high)
+     */
+    LimitSwitch(const int pin);
+
+    /**
+     * Updates the limit switch. Must be called EVERY loop cycle.
+     */
+    void Update();
+
+    /**
+     * Returns the value of the limit switch found at the last
+     * update
+     * @return If the limit switch is currently pushed
+     */
+    bool Get();
+
+    /**
+     * Returns whether the limit switch was newly pushed in this
+     * update cycle
+     * @return If the limit switch has been newly pushed down
+     */
+    bool Pushed();
+
+    /**
+     * Returns whether the limit switch was newly released in this
+     * update cycle
+     * @return If the limit switch has been newly released
+     */
+    bool Released();
+
+   private:
+    const int mPin;
+    bool mState;
+    bool mLast;
+};
+}  // namespace lattice
