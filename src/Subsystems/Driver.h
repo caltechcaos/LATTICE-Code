5638fed5be78a11e8e907bc7223cfee80b4a7a00
winterbloom <23458874+winterbloom@users.noreply.github.com>
2022-08-22 17:22:09 -0700

    Basic driver framework

---
+++ src/Subsystems/Driver.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "LimitSwitch.h"
+#include "Thermistor.h"
+#include "HandoffMotor.h"
+#include "CurrentSensor.h"
+
+namespace lattice {
+    class Driver {
+        public:
+        Driver();
+
+        private:
+        // ElevatorMotor elevator;
+        // HytorcMotor actuator;
+        HandoffMotor handoff;
+
+        // RCInput rc_input;
+        // Logger rc_output;
+
+        LimitSwitch first_stake;
+        LimitSwitch second_stake;
+        LimitSwitch third_stake;
+
+        LimitSwitch elevator_zero; // top
+        LimitSwitch elevator_end; // bottom
+
+        Thermistor actuator_temp;
+        CurrentSensor actuator_current;
+        CurrentSensor driver_current;
+    };
+}
