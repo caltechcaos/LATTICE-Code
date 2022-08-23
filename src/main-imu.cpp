bc5f1b2b2e474aa236ff61f6b41e0c713b6402ac
Matteo Kimura <matteohkimura@gmail.com>
2022-08-22 17:40:47 -0700

    Add IMU Driver

---
+++ src/main-imu.cpp
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+
+#include "IMU.h"
+#include "Util.h"
+
+lattice::IMU imu1;
+lattice::IMU imu2{lattice::IMU::WireType::kOne};
+
+void setup() {
+    lattice::GenericSetup();
+    imu1.Setup();
+    imu2.Setup();
+}
+
+void printIMU(lattice::IMU imu) {
+    Serial.print(imu.GetYaw());
+    Serial.print(", ");
+    Serial.print(imu.GetPitch());
+    Serial.print(", ");
+    Serial.print(imu.GetRoll());
+    Serial.print(", ");
+    Serial.print(imu.GetXAccel());
+    Serial.print(", ");
+    Serial.print(imu.GetYAccel());
+    Serial.print(", ");
+    Serial.print(imu.GetZAccel());
+    Serial.println();
+}
+
+void loop() {
+    // printIMU(imu1);
+    printIMU(imu2);
+}
