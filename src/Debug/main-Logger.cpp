d821dc79932619105dba44c5b377becf3764928e
winterbloom <23458874+winterbloom@users.noreply.github.com>
2022-09-07 13:34:44 +0100

    Expands logger test

--- src/Debug/main-Logger.cpp
+++ src/Debug/main-Logger.cpp
@@ -6,4 +6,13 @@ void setup() {
 }
 
 void loop() {
+    typedef lattice::Logger::Priority P;
+    typedef lattice::Logger::ErrorCode E;
+    lattice::Logger &logger = lattice::Logger::logger();
+
+    logger.Log(P::Verbose, E::Message, "verbose");
+    logger.Log(P::Log, E::Message, "Log.");
+    logger.Log(P::Warning, E::Message, "Warning");
+    logger.Log(P::Error, E::Message, "Error!!");
+    delay(10000); // 10 seconds
 }
