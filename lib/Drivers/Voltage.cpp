639f9cd75e461ff3d197b897210b8f4d809b5c26
Ceyx22 <fm278841@gmail.com>
2022-09-10 17:54:08 -0700

    working on resistor code 12 bit making it difficult

---
+++ lib/Drivers/Voltage.cpp
@@ -0,0 +1,17 @@
+#include "Voltage.h"
+#include <Arduino.h>
+
+using namespace lattice;
+
+Voltage::Voltage(const int PIN) : M_PIN(PIN) { }
+ double temp = 0;
+void Voltage::Setup() {
+    pinMode(M_PIN, INPUT);
+}
+double Voltage::CalculateVoltage(double MaxInputVoltage, double Multiplier) {
+    //digitalWrite(M_PIN, value ? HIGH : LOW);
+    ReadValue = analogRead(M_PIN);
+    CurrentVoltage = (ReadValue * MaxInputVoltage) ;
+    //temp = (CurrentVoltage * Multiplier) / Divisor;
+    return CurrentVoltage;
+}
--- src/Util.h
+++ src/Util.h
@@ -3,6 +3,7 @@
 #undef min
 #undef max
 #include <string>
+#include <Arduino.h>
 
 /**
  * Contains all generic utility functions for the main classes
@@ -77,6 +78,10 @@ constexpr double kA = 0;
 
 namespace ShuttleConstants{
  constexpr int kBrakePin = 0;
+ //This have to be analogPin and cannot exceed 5V
+ constexpr int kVoltagePin = A0; 
+ constexpr double kMaxInputVoltage = 1.2; //temp (dependson fully charge bat and resistor)
+ constexpr double kMultiplier = 3.6; //temp (depends on how much of the origional voltage is inputed)(I.E. 1/3)
 }
 
 
