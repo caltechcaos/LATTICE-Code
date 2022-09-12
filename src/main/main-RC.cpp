cacbb0cbe1e51ad92174449493ae491380751353
ericlovesmath <ericlovesmath@gmail.com>
2022-09-12 16:03:16 -0700

    RC Control function for Clifford, Elevator, and Shuttle

--- src/main/main-RC.cpp
+++ src/main/main-RC.cpp
@@ -1,12 +1,17 @@
 #include <Arduino.h>
 #include "RC.h"
 #include "Subsystems/Clifford.h"
+#include "Subsystems/Shuttle.h"
 #include "Subsystems/Driver.h"
 #include "Util.h"
 
-lattice::RC controller(Serial1, 19, 2, 13);
-auto& clifford = lattice::Clifford::clifford();
-auto& driver = lattice::Driver::driver();
+#define RPMSCALE 5700
+
+lattice::RC controller(Serial1, 19, 2, 13); // TODO: Use correct wiring wjen ATVR is fixed
+lattice::Clifford &clifford = lattice::Clifford::clifford();
+lattice::Driver &driver = lattice::Driver::driver();
+lattice::Shuttle &shuttle = lattice::Shuttle::GetInstance();
+lattice::HytorcSimple hytorcSimple{9, 5, 6}; // TODO: Temp Ports
 
 // Runs at init
 void setup() {
@@ -15,30 +20,68 @@ void setup() {
     driver.Setup();
 }
 
-// RUns continously
+// Vertical: Move Drive Train Up/Down
+// Horizontal: Move Drive Train Left/Right
+bool updateClifford(double x, double y) {
+    clifford.Move(x, y);
+    return true;
+}
+
+// Vertical: Move Elevator
+// Horizontal: Switch Stake
+bool updateElevator(double y, double drill) {
+    // driver.SetElevatorPower(y); TODO: Add once implemented
+    hytorcSimple.SetPercentOutput(drill);
+    return true;
+}
+
+// Horizontal: Drive Left/Right
+bool updateShuttle(double x) {
+    shuttle.SetMotionMotors(x * RPMSCALE);
+    return true;
+}
+
+// Runs continously
 void loop() {
     controller.Update();
 
-    // Clifford Translational Motion
-    double x = controller.GetThrottle();
+    // Translational Motion, Right side
+    double x = controller.GetAileron();
     double y = controller.GetElevator();
-    clifford.Move(x, y);
+    double drill = (controller.GetRudder() - 0.5) * 2;
+    int stake = controller.GetAux2();
 
-    // Driver 
-    double x_Driver = controller.GetAileron(); // hand off motor
-    double y_Driver = controller.GetRudder(); // elevator motor
-    
-    
-    // Switch
-    switch (controller.GetAux2()) {
+    // Set stake number with Switch A, assume there's 3 for now
+    switch (stake) {
         case 0:
-            driver.ZeroElevator();
+            driver.SetStake(lattice::Driver::StakeNumber::kOne);
+            break;
+        case 1:
+            driver.SetStake(lattice::Driver::StakeNumber::kTwo);
+            break;
+        case 2:
+            driver.SetStake(lattice::Driver::StakeNumber::kThree);
+            break;
+    }
+        
+    // F Switch Finite State Machine
+    bool success;
+    switch (controller.GetGear()) {
+        case 0:
+            success = updateClifford(x, y);
+            break;
+        case 1:
+            success = updateElevator(y, drill);
+            break;
+        case 2:
+            success = updateShuttle(x);
             break;
         default:
-            driver.SetStake(lattice::Driver::StakeNumber::kOne);
+            success = false;
+            Serial.print("Unexpected return val for controller.GetGear()");
+    };
+
+    if (!success) {
+        Serial.print("Failure in Finite State Machine for RC");
     }
 }
-
-
-// TODO: Elevator controls
-// TODO: Switch (Planting, Drilling)
