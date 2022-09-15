4b8e6fb25c4b47fa0054b69e1f25c26da1bf88cf
Ceyx22 <fm278841@gmail.com>
2022-09-15 00:14:33 -0700

    messing with stepper code

--- src/Debug/main-StepperTest.cpp
+++ src/Debug/main-StepperTest.cpp
@@ -1,23 +1,24 @@
 #include <Arduino.h>
-#include <Stepper.h>
+#include <AccelStepper.h>
 #include "Util.h"
 //#include "HandoffMotor.h"
 
 using namespace lattice;
-using namespace lattice::HandoffConstants;
+//using namespace lattice::HandoffConstants;
 
 //lattice::HandoffMotor StepperDriver(kDrivePin, kStepPin, kStepsPerRev);//, kRPM, kMicrosteps
 
 int stepsPerRevolution = 200;
-
-Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
-int steps; 
+//AccelStepper stepper = AccelStepper(MotorInterfaceType, 8, 9, 10, 11);
+AccelStepper myStepper(4, 8, 9, 10, 11);
+int Speed; 
 
 void setup() {
     lattice::GenericSetup();
+    myStepper.setMaxSpeed(1000);
     //StepperDriver.Setup();
     //StepperDriver.SetAccel(kMotorAccel, kMotorDecel);
-    myStepper.setSpeed(60);
+    //myStepper.setSpeed(60);
 }
 
 void loop() {
@@ -25,20 +26,21 @@ void loop() {
         char input = (uint8_t)Serial.read();
 
         if (input == 'a') {
-            steps = 700;
+            Speed = 50;
         } else if (input == 'b') {
-            steps = -700;
+            Speed = -50;
         } else if (input == 'c') {
-            steps = 1400;
+            Speed = 100;
         } else if (input == 'd') {
-            steps = -1400;
+            Speed = -100;
         } else if (input == 'f') {
-            steps = 0;
+            Speed = 0;
         }
 
     }
+    myStepper.setSpeed(Speed);
     //StepperDriver.StepMove(steps);    
-    myStepper.step(steps);
-    Serial.println(steps);
+    myStepper.runSpeed();
+    Serial.println(Speed);
 
 }
